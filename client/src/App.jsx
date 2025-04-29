bfdde657f71edc9d0e3caa2079942d2f7eaae99f
Indra Nurzaman <nurzamanindra@gmail.com>
2025-04-29 15:17:02 +0700

    add pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
 const App = () => {
   return (
-    <section className='container mx-auto p-36 text-center'>
-      app
-    </section>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
