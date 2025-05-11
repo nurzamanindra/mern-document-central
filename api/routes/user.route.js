018c2dd85165a8748ce729eda14496fd4806f9ef
Indra Nurzaman <nurzamanindra@gmail.com>
2025-05-11 13:04:14 +0700

    add BE delete user by id routes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {updateUserDetails} = require('../controllers/user.controller');
+const {updateUserDetails, deleteUser} = require('../controllers/user.controller');
 const { protect } = require('../middleware/auth');
 
 
 const router = express.Router();
 
-// @route     PUT /api/v1/user/update/:userId
 router.put('/update/:userId', protect, updateUserDetails);
+router.delete('/delete/:userId', protect, deleteUser);
 
 
 module.exports = router;
